c816432c0683f0598606a7700a97e6eb984cfa8e
Krishendree Kistensamy <krishendree@gmail.com>
2026-05-27 14:14:23 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,9 +1,27 @@
 // Create a function called greet that takes a name parameter
+// TODO: Make the greet function return "Hello, [name]!"
 function greet(name) {
     return "Hello, " + name + "!";
 }
 
 // Create a function called add that takes two parameters
+// TODO: Make the add function return the sum of both parameters
 function add(x, y) {
     return x + y;
 }
+
+// TODO: Call both functions and store their results in variables
+// TODO: Print the results to the console
+let greeting = greet("Alice");
+let sum = add(5, 10);
+
+console.log(greeting); //hello, Alice
+console.log("Total is: " + sum); //15
+
+
+
+// Owam - Created a function called greet that takes a name parameter
+// Created a function called add that takes two parameters
+
+// Krishendree - called both functions and stored their variables 
+// Printed the reults to the console and double checked the code for a perfect function and result
