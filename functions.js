0ff8f3dc5d1eca93af7d8fa97e11c2ec50ed1304
Owam Gobinca <owamgobingca@gmail.com>
2026-05-27 03:35:12 -0700

    inital commit

---
+++ functions.js
@@ -0,0 +1,9 @@
+// Create a function called greet that takes a name parameter
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+
+// Create a function called add that takes two parameters
+function add(x, y) {
+    return x + y;
+}
